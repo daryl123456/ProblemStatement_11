0adb2ace8d1215687471e1ba2aa81d50d0024ced
Nicole koo <19047433@myrp.edu.sg>
2021-07-29 16:48:37 +0800

    enhancement-NICOLE

--- app/src/main/java/sg/edu/rp/c346/id19042545/problemstatement11/ScheduledNotificationReceiver.java
+++ app/src/main/java/sg/edu/rp/c346/id19042545/problemstatement11/ScheduledNotificationReceiver.java
@@ -7,6 +7,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -33,11 +36,16 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
         Intent i = new Intent(context,MainActivity.class);
         PendingIntent pIntent =  PendingIntent.getActivity(context,reqCode,i,PendingIntent.FLAG_CANCEL_CURRENT);
 
-
+        Bitmap picture = BitmapFactory.decodeResource(context.getResources(), R.drawable.sentosa);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"default");
         builder.setContentTitle(name);
         builder.setContentText(desc);
         builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setVibrate(new long[]{1000});
+        builder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(picture)
+                .bigLargeIcon(null)
+        );
         builder.setContentIntent(pIntent);
         builder.setAutoCancel(true);
 
@@ -45,4 +53,5 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
         notificationManager.notify(123,n);
 
     }
+
 }
